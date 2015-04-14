761c207089292dae48d4e42c6bb3d2524e10e86f
moorcroftlad <moorcroftlad@hotmail.com>
2015-04-14 18:56:40 +0100

    persists chat in .net cache keyed on IP

--- LocalChat/SignalR/ChatHub.cs
+++ LocalChat/SignalR/ChatHub.cs
@@ -1,12 +1,82 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.AspNet.SignalR;
 
 namespace LocalChat.SignalR
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string username, string message)
         {
-            Clients.All.broadcastMessage(name, message);
+            var chatRoom = RetrieveChatRoom();
+            var chatMessage = new ChatMessage(username, message);
+            chatRoom.AddMessage(chatMessage);
+            UpdateChatRoom(chatRoom);
+            Clients.All.broadcastMessage(username, message);
         }
+
+        public void UpdateChatHistory()
+        {
+            var chatRoom = RetrieveChatRoom();
+            Clients.Caller.updateChatHistory(chatRoom.RetrieveAll());
+        }
+
+        private ChatRoom RetrieveChatRoom()
+        {
+            var ip = GetClientIp(Context.Request);
+            return HttpRuntime.Cache[ip] as ChatRoom ?? new ChatRoom();
+        }
+
+        private void UpdateChatRoom(ChatRoom chatRoom)
+        {
+            var ip = GetClientIp(Context.Request);
+            HttpRuntime.Cache.Insert(ip, chatRoom);
+        }
+
+        private static string GetClientIp(IRequest request)
+        {
+            return Get<string>(request.Environment, "server.RemoteIpAddress");
+        }
+
+        private static T Get<T>(IDictionary<string, object> env, string key)
+        {
+            object value;
+            return env.TryGetValue(key, out value) ? (T)value : default(T);
+        }
+    }
+
+    public class ChatRoom
+    {
+        private readonly List<ChatMessage> _messages;
+
+        public ChatRoom()
+        {
+            _messages = new List<ChatMessage>();
+        }
+
+        public void AddMessage(ChatMessage chatMessage)
+        {
+            _messages.Add(chatMessage);
+        }
+
+        public List<ChatMessage> RetrieveAll()
+        {
+            return _messages;
+        }
+    }
+
+    public class ChatMessage
+    {
+        public ChatMessage(string username, string message)
+        {
+            Username = username;
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+
+        public string Username { get; private set; }
+        public string Message { get; private set; }
+        public DateTime Timestamp { get; private set; }
     }
 }
--- LocalChat/assets/js/chat/chat.js
+++ LocalChat/assets/js/chat/chat.js
@@ -1,15 +1,23 @@
 $(function () {
     var chat = $.connection.chatHub;
+
     chat.client.broadcastMessage = function (name, message) {
         var encodedName = $('<div />').text(name).html();
         var encodedMsg = $('<div />').text(message).html();
         $('#discussion').append('<li><strong>' + encodedName + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
     };
     
+    chat.client.updateChatHistory = function (messages) {
+        $.each(messages, function(key, message) {
+            $('#discussion').append('<li><strong>' + message.Username + '</strong>:&nbsp;&nbsp;' + message.Message + '</li>');
+        });
+    };
+    
     $('#displayname').val(prompt('Enter your name:', ''));
     $('#message').focus();
     
     $.connection.hub.start().done(function () {
+        chat.server.updateChatHistory();
         $('#sendmessage').click(function () {
             chat.server.send($('#displayname').val(), $('#message').val());
             $('#message').val('').focus();
